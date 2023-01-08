fb24c4736a2279660155f998ac9c06271dfc6712
katerinaklapuchova <katerina.klapuchova@skrz.cz>
2023-01-08 15:39:53 +0100

    renamed cpu to cpu_usage

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE servers
 (
   id serial PRIMARY KEY,
   name text UNIQUE NOT NULL CHECK (trim(name) != ''),
-  cpu int NOT NULL CHECK (cpu >= 0),
-  memory_usage int NOT NULL CHECK (cpu >= 0),
+  cpu_usage int NOT NULL CHECK (cpu_usage >= 0),
+  memory_usage int NOT NULL CHECK (cpu_usage >= 0),
   created_at timestamp with time zone NOT NULL,
   status text NOT NULL CHECK (status IN ('Running', 'Stopped')),
   ip_address text[] NOT NULL

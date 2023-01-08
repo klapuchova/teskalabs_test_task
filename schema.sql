bf4c04217e46c9a98449494f5bd19a4d5dcdb0dc
katerinaklapuchova <katerina.klapuchova@skrz.cz>
2023-01-08 15:34:38 +0100

    added constraints

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE servers
 (
   id serial PRIMARY KEY,
-  name text UNIQUE NOT NULL,
-  cpu int NOT NULL,
-  memory_usage int NOT NULL,
+  name text UNIQUE NOT NULL CHECK (trim(name) != ''),
+  cpu int NOT NULL CHECK (cpu >= 0),
+  memory_usage int NOT NULL CHECK (cpu >= 0),
   created_at timestamp with time zone NOT NULL,
-  status text NOT NULL,
+  status text NOT NULL CHECK (status IN ('Running', 'Stopped')),
   ip_address text[] NOT NULL
 );

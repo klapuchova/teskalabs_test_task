03d860e45fa4ae8de6e70fdeca4142c322036193
katerinaklapuchova <katerina.klapuchova@skrz.cz>
2023-01-08 15:20:36 +0100

    formatted schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,10 @@
-CREATE TABLE servers(id serial primary key ,
-                     name text unique,
-                     cpu int,
-                     memory_usage int,
-                     created_at timestamp with time zone,
-                     status text,
-                     ip_address text[]
+CREATE TABLE servers
+(
+  id serial PRIMARY KEY,
+  name text UNIQUE,
+  cpu int,
+  memory_usage int,
+  created_at timestamp with time zone,
+  status text,
+  ip_address text[]
 );

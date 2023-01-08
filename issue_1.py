d2bc673359ace451a38c0adc89b535a14963f3eb
katerinaklapuchova <katerina.klapuchova@skrz.cz>
2023-01-08 16:07:13 +0100

    double quotes to single quotes

--- issue_1.py
+++ issue_1.py
@@ -29,26 +29,26 @@ loaded_data = load_source_data()
 
 
 for row in loaded_data:
-    name = row["name"]
+    name = row['name']
 
     try:
-        cpu_usage = row["state"]["cpu"]["usage"]
+        cpu_usage = row['state']['cpu']['usage']
     except TypeError:
         continue
 
-    memory_usage = row["state"]["memory"]["usage"]
-    created_at = row["created_at"]
-    status = row["status"]
+    memory_usage = row['state']['memory']['usage']
+    created_at = row['created_at']
+    status = row['status']
 
 
-    addresses = row["state"]["network"]
+    addresses = row['state']['network']
     api_lines = []
     all_api_address = []
     for i in addresses:
-        dict_name_paths = row["state"]["network"][i]["addresses"]
+        dict_name_paths = row['state']['network'][i]['addresses']
 
         for path in dict_name_paths:
-            api_lines.append(path["address"])
+            api_lines.append(path['address'])
     all_api_address.extend(api_lines)
 
     api_address = all_api_address
@@ -65,7 +65,7 @@ for row in loaded_data:
 
     conn = connection(postgres_login.host, postgres_login.database, postgres_login.user, postgres_login.password)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM servers WHERE name=%s AND memory_usage=%s", (name, memory_usage))
+    cur.execute('SELECT * FROM servers WHERE name=%s AND memory_usage=%s', (name, memory_usage))
     if cur.rowcount == 0:
         cur.execute("""INSERT INTO servers (name, cpu_usage, memory_usage, created_at, status, ip_address) VALUES (%s, %s, %s, to_timestamp(%s, 'YYYY-MM-DD"T"HH24:MI:SSTZH:TZM')::timestamptz, %s, %s)""",
                     (name, cpu_usage, memory_usage, created_at, status, api_address))

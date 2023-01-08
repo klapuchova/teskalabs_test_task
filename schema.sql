4d2f406ccc55e207d5d7cae5fe2ae49f916d1dd4
katerinaklapuchova <katerina.klapuchova@skrz.cz>
2023-01-08 15:40:53 +0100

    fixed check for memory usage

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE servers
   id serial PRIMARY KEY,
   name text UNIQUE NOT NULL CHECK (trim(name) != ''),
   cpu_usage int NOT NULL CHECK (cpu_usage >= 0),
-  memory_usage int NOT NULL CHECK (cpu_usage >= 0),
+  memory_usage int NOT NULL CHECK (memory_usage >= 0),
   created_at timestamp with time zone NOT NULL,
   status text NOT NULL CHECK (status IN ('Running', 'Stopped')),
   ip_address text[] NOT NULL
